844c78dedf8f047f002313abee45268a3e47cd88
Liltooly <francoerr14@gmail.com>
2021-08-02 00:05:52 -0300

    Login with eye function complete

--- Page Master/Login/main.js
+++ Page Master/Login/main.js
@@ -2,8 +2,10 @@ function seePassword(){
     var see= document.getElementById("loginPassword");
     if(see.type == "password"){
         see.type = "text";
+        document.querySelector('.seePass').style.backgroundImage = "url(css/hidepass.svg)";
     }else{
         see.type = "password";
+        document.querySelector('.seePass').style.backgroundImage = "url(css/showpass.svg)";
     }
 }
 
