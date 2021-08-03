09571ead1745429bc5f8573d5708b0d619dbe073
Liltooly <francoerr14@gmail.com>
2021-08-03 13:53:27 -0300

    Adding Main page and Login almost finished

--- Page Master/Login/main.js
+++ Page Master/Login/main.js
@@ -1,11 +1,10 @@
 function seePassword(){
-    var see= document.getElementById("loginPassword");
-    if(see.type == "password"){
+    var see = document.getElementById("loginPassword");
+    if(see.type == "password") {
         see.type = "text";
         document.querySelector('.seePass').style.backgroundImage = "url(css/hidepass.svg)";
-    }else{
-        see.type = "password";
-        document.querySelector('.seePass').style.backgroundImage = "url(css/showpass.svg)";
+        return;
     }
+    see.type = "password";
+    document.querySelector('.seePass').style.backgroundImage = "url(css/showpass.svg)";
 }
-
