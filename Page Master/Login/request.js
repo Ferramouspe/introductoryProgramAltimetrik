fcc9a2905b19749f339e1c6d4f7d7d18a78bc8b0
Liltooly <francoerr14@gmail.com>
2021-08-22 19:14:35 -0300

    Main almost ready

--- Page Master/Login/request.js
+++ Page Master/Login/request.js
@@ -1,1 +1,12 @@
-fetch()
+function validLogin() {
+    let email = document.getElementById("loginUsername");
+    let password = document.getElementById("loginPassword");
+    fetch("http://localhost:3000/login", {method: "post", mode: "cors", 
+    headers: {'Content-Type': 'application/json'}, 
+    body: JSON.stringify({"email": email, "password": password})}).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+    return false;
+}
