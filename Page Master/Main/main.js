75dfa0fc170e143abcdb4fe8d5f58e905c6a728a
Franco Erramouspé <86250679+Liltooly@users.noreply.github.com>
2021-09-26 22:03:21 -0300

    last version without debug

--- Page Master/Main/main.js
+++ Page Master/Main/main.js
@@ -1,27 +1,19 @@
 function switch3to1() {
         let enabled = "#515151";
         let disabled = "#303030";
-        let columns3 = document.querySelector('.columns3SVG').style.backgroundColor;
-        let column1 = document.querySelector('.column1SVG').style.backgroundColor;
+        let columns3 = document.getElementById("columns3Id").style.backgroundColor;
+        let column1 = document.getElementById("column1Id").style.backgroundColor;
         
-        if(columns3 == disabled){
-            columns3 = enabled;
-            column1 = disabled;
-        }else{
-            columns3 = enabled;
-        }
+        columns3 = enabled;
+        column1 = disabled;
 }
 
 function switch1to3() {
     let enabled = "#515151";
     let disabled = "#303030";
-    let columns3 = document.querySelector('.columns3SVG').style.backgroundColor;
-    let column1 = document.querySelector('.column1SVG').style.backgroundColor;
+    let columns3 = document.getElementById("columns3Id").style.backgroundColor;
+    let column1 = document.getElementById("column1Id").style.backgroundColor;
     
-    if(column1 == disabled){
-        column1 = enabled;
-        columns3 = disabled;
-    }else{
-        column1 = enabled;
-    }
+    column1 = enabled;
+    columns3 = disabled;
 }
