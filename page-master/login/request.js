449da260ea7f6a92a072232c671daba2b142c778
Franco Erramouspe <ferramouspe@altimetrik.com>
2021-11-05 02:32:13 -0300

    last changes

--- page-master/login/request.js
+++ page-master/login/request.js
@@ -1,36 +1,28 @@
-// document.addEventListener("load", onLoad);
-
-// function onLoad(){
 document.getElementById("loginButtonId").addEventListener("click", validLogin);
-// }
-async function validLogin(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    let email = document.getElementById("loginUsername").value;
-    const emailRegEx = /^[a-zA-Z0-9.-]+@[a-zA-Z0-9]+\.[a-zA-Z]+[a-zA-Z]*$/;
-    let password = document.getElementById("loginPassword").value;
-    const passRegEx = /^[a-zA-Z0-9.-]{3,16}$/;
-    
-    let emailValidation = emailRegEx.test(email.value);
-    let passValidation = passRegEx.test(password.value); 
-    if(true){
-        let fetchResponse = await fetch("http://localhost:3000/users", {method: "GET", mode: "cors", 
-        headers: {'Content-Type': 'application/json'}, 
-        body: JSON.stringify({"email": email, "password": password})});
-        let jsonResponse = await fetchResponse.json();
 
-        if(jsonResponse.status === 200 || 201){
-        /*  document.cookie = "authTocken-" + jsonResponse.accessToken; */
-            /* window.location = "../Main/main.html"; */
-            console.log("entra");
-            return;
-        }
-        noUser();
-        return;
+function validLogin() {
+    let email = document.getElementById("loginUsername").value;
+    const emailRegEx = /^[a-zA-Z0-9\.-]+@[a-zA-Z0-9]+\.[a-zA-Z]+[a-zA-Z]*$/;
+    let password = document.getElementById("loginPassword").value;
+    const passRegEx = /^[a-zA-Z0-9\.-]{3,16}$/;
+    
+    let emailValidation = emailRegEx.test(email);
+    let passValidation = passRegEx.test(password); 
+    if(emailValidation && passValidation) {
+        fetch("http://localhost:3000/login", {method: "POST", mode: "cors", 
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify({"email": email, "password": password})})
+        .then( (response) => {
+            if(response.status === 200 || response.status === 201){
+                response.json().then( (responseJson) => { 
+                    window.sessionStorage.setItem("token",responseJson.accessToken);
+                    window.location = "../main/main.html";
+                })   
+            }else noUser();
+        })
     }
-    console.log("no entra");
 }
 
 function noUser(){
-    console.log("No Entraste Man");
+    console.log("Invalid User");
 }
